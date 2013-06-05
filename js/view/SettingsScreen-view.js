b43b714ade1ab908bc8d26ac6ec64f9557a523fd
Pavlov Max <m.a.pavlov.tver@gmail.com>
2013-06-05 17:43:24 +0400

    Added multiscreen menu screen feature

--- js/view/SettingsScreen-view.js
+++ js/view/SettingsScreen-view.js
@@ -4,6 +4,7 @@ define(["view/Screen-view"], function(ScreenView) {
 		template: _.template($('#settingsmenu-template').html()),
 		initialize: function() {
 			this.activeItemIndex = 0;
+			this.itemsPerScreen = 8;
 			this.items = this.options.items;
 			this.on("button.left", this.onLeftButton);
 			this.on("button.middle", this.onMiddleButton);
@@ -14,15 +15,23 @@ define(["view/Screen-view"], function(ScreenView) {
 		},
 		render: function() {
 			this.$el.html(this.template());
+			var currentScreen = this.getItemScreenIndex();
+			var startIndex = currentScreen * this.itemsPerScreen;
+			var endIndex = startIndex + this.itemsPerScreen;
 			_.each(this.items, function(item, i) {
-				var renderedItem = $(item.render().$el);
-				i == this.activeItemIndex ? renderedItem.addClass('active') : renderedItem.removeClass('active');
-				this.$el.find('.menu').append(renderedItem);
+				if(i >= startIndex && i < endIndex) {
+					var renderedItem = $(item.render().$el);
+					i == this.activeItemIndex ? renderedItem.addClass('active') : renderedItem.removeClass('active');
+					this.$el.find('.menu').append(renderedItem);
+				}
 			}, this);
 			this.eventBus.trigger("device.panel.leftButton", "up");
 			this.eventBus.trigger("device.panel.middleButton", "select");
 			this.eventBus.trigger("device.panel.rightButton", "down");
 			return this;
+		},
+		getItemScreenIndex: function() {
+			return Math.floor(this.activeItemIndex / this.itemsPerScreen);
 		},
 		onLeftButton: function() {
 			this.activeItemIndex--;
